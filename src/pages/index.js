63852bdc8e8437eccf040e8bc5e1f8d617dca289
Eduardo dos Santos Leal <eduardoleal1981@gmail.com>
2022-11-30 22:56:47 -0300

    Configuração inicial

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/story-artist">
-            Docusaurus Tutorial - 5min ⏱️
+            Curso: Grease Pencil Fundamentals  - 5min ⏱️
           </Link>
         </div>
       </div>
